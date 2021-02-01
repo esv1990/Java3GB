20f8d2060eb85ae92d8868e064ba17664b2319d4
esv1990 <sergserg081990@yandex.ru>
2021-02-01 19:43:04 +0400

    hw5

---
+++ Lesson5/Main.java
@@ -0,0 +1,18 @@
+package Lesson5;
+
+public class Main {
+
+    public static final int CARS_COUNT = 5;
+
+    public static void main(String[] args) {
+
+        Stage[] stages = {
+                new Road(60),
+                new Tunnel(CARS_COUNT / 2),
+                new Road(40)
+        };
+
+        Race race = new Race(stages, CARS_COUNT);
+        race.start();
+    }
+}
